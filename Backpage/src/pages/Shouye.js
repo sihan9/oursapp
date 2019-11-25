68f90a2a3863668008207e844ea03dfea4c39f21
Fanningyuan <1789624333qq.com>
2019-11-25 17:15:34 +0800

    backpage

---
+++ Backpage/src/pages/Shouye.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Shouye extends Component {
+    render() {
+        return (
+            <div className='shouye-1'>
+                
+            </div>
+        )
+    }
+}
