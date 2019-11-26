5989816d7b6f4f7593a25cfd2aa7c0281b8bbe21
Fanningyuan <1789624333qq.com>
2019-11-26 09:01:41 +0800

    后台原型（未完成版）

--- Backpage/src/pages/Shouye.js
+++ Backpage/src/pages/Shouye.js
@@ -4,7 +4,21 @@ export default class Shouye extends Component {
     render() {
         return (
             <div className='shouye-1'>
-                
+                <div className='shouye-1-2'>
+                    今日累计注册量30000
+                </div>
+                <div className='shouye-1-2' style={{marginLeft:'80px'}}>
+                    今日访问量20000
+                </div>
+                <div style={{width:'550px',height:'215px',backgroundColor:'green',float:'left'}}>
+                    图片
+                </div>
+                <div style={{width:'550px',height:'215px',backgroundColor:'yellow',float:'left'}}>
+                    图片
+                </div>
+                <div className='shouye-1-2' style={{marginLeft:'80px'}}>
+                    注册量20000
+                </div>
             </div>
         )
     }
