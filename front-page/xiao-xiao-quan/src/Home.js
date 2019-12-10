869e6cdde0926bdac27bb8e6e87d39ae305c4ad7
褚雅琦 <33738288+chuyaqi6@users.noreply.github.com>
2019-12-10 16:50:49 +0800

    Update Home.js

--- front-page/xiao-xiao-quan/src/Home.js
+++ front-page/xiao-xiao-quan/src/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router,Route,Switch,Redirect} from 'react-router-dom';
+import {HashRouter as Router,Route,Switch,Redirect} from 'react-router-dom';
 import App from './App';
 import Login from './container/Login';
 import Set from './container/Set';
