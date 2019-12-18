80dee80aa500344969281e7f2aae74212fc11b81
chuyaqi6 <2484917266@qq.com>
2019-12-18 10:12:22 +0800

    add Node api

---
+++ Node api/Routes/showdata.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('./pg');
+
+function showdata (res,sql){
+    pool.query(sql, (error,results,fields)=> {
+        if (error) console.log(error.message);
+        // console.log(sql);
+        console.log(results);
+        //对象解析为json字符串// results = JSON.stringify(results);
+        var db = { state: 200, message: '获取成功', content: results.rows };
+        var data = JSON.stringify(db);
+        console.log(data);
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.writeHead(200,{'Content-Type':'text/plain;charset="utf-8"'});
+        res.end(data);
+        console.log(data);
+    });
+}
+module.exports = showdata;
